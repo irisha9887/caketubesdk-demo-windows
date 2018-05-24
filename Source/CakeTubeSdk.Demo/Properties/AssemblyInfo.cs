8e1534d103c007373e3ff4883698a03abbeaf73d
Aleksander Kravtsov <krav.gear@gmail.com>
2018-05-24 14:49:06 +0700

    build 1.0.17.17

--- Source/CakeTubeSdk.Demo/Properties/AssemblyInfo.cs
+++ Source/CakeTubeSdk.Demo/Properties/AssemblyInfo.cs
@@ -12,5 +12,5 @@ using System.Windows;
 [assembly: ComVisible(false)]
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
 
-[assembly: AssemblyVersion("1.0.16.16")]
-[assembly: AssemblyFileVersion("1.0.16.16")]
+[assembly: AssemblyVersion("1.0.17.17")]
+[assembly: AssemblyFileVersion("1.0.17.17")]
